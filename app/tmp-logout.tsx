dde9d7537e69833a17f5a1fba374a4fabdf50ab2
shwetank-nitp <sweetankr.ug23.ee@nitp.ac.in>
2026-03-26 20:53:53 +0530

    implemented protected trpc protectedProcedure

---
+++ app/tmp-logout.tsx
@@ -0,0 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+
+export default function LogoutButton() {
+  async function logout() {
+    authClient.signOut(
+      {},
+      {
+        onSuccess: function () {
+          toast.info("Logout successfully");
+        },
+
+        onError: function (ctx) {
+          toast.error("Something went wrong while logging out");
+          console.error("Logout error", ctx.error);
+        },
+      }
+    );
+  }
+
+  return <Button onClick={() => logout()}>Logout</Button>;
+}
--- trpc/trpc.ts
+++ trpc/trpc.ts
@@ -1,12 +1,17 @@
-import { initTRPC } from "@trpc/server";
+import { auth } from "@/lib/auth";
+import { initTRPC, TRPCError } from "@trpc/server";
+// import { headers } from "next/headers";
 /**
  * This context creator accepts `headers` so it can be reused in both
  * the RSC server caller (where you pass `next/headers`) and the
  * API route handler (where you pass the request headers).
  */
 export const createTRPCContext = async (opts: { headers: Headers }) => {
-  // const user = await auth(opts.headers);
-  return { userId: "user_123" };
+  const session = await auth.api.getSession({
+    headers: opts.headers,
+  });
+
+  return { session };
 };
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
@@ -24,3 +29,16 @@ const t = initTRPC
 export const createTRPCRouter = t.router;
 export const createCallerFactory = t.createCallerFactory;
 export const baseProcedure = t.procedure;
+
+export const protectedProcedure = baseProcedure.use(({ ctx, next }) => {
+  if (!ctx.session) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  return next({
+    ctx: {
+      ...ctx,
+      userId: ctx.session, // now typed as non-null downstream
+    },
+  });
+});
